78ed044fb2486fb3f3e072701c4b00a38ebfafbb
Mohammad Mahdi <mmanoosheh@gmail.com>
2026-07-03 18:48:28 +0330

    starting project

---
+++ Game.py
@@ -0,0 +1,14 @@
+class GameObject:
+    pass
+class Player(GameObject):
+    pass
+class Target(GameObject):
+    pass
+class Item(Target):
+    pass
+class Gun(GameObject):
+    pass
+class GameManager(GameObject):
+    pass
+class GUIElements(GameObject):
+    pass
